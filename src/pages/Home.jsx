ca8774a6ae15ad5a1ac864336bd1714728b69c30
charleslukes <charleslukes28@gmail.com>
2023-09-27 19:42:03 +0100

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Sidebar from '../components/Sidebar';
+import DefaultLayout from '../layout/DefaultLayout';
+
+const Home = () => {
+
+  return (
+    <div>
+        <DefaultLayout />
+    </div>
+  );
+};
+
+export default Home;
