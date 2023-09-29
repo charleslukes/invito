e245d5117a7e6ec73f14e6617aed413980f6834c
charleslukes <charleslukes28@gmail.com>
2023-09-29 04:45:25 +0100

    update dashboard layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "@solidjs/router";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import { createSignal } from "solid-js";
+import Table from "../components/Table";
+import Card from "../components/Card";
 
 const DefaultLayout = () => {
-    const [sidebarOpen, setSidebarOpen] = createSignal(true);
+  const [sidebarOpen, setSidebarOpen] = createSignal(true);
 
-    console.log(sidebarOpen)
+  console.log(sidebarOpen);
 
   return (
     <div class="dark:bg-boxdark-2 dark:text-bodydark">
@@ -19,20 +21,25 @@ const DefaultLayout = () => {
         {/* <!-- ===== Content Area Start ===== --> */}
         <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           {/* <!-- ===== Header Start ===== --> */}
-           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           {/* <!-- ===== Header End ===== --> */}
 
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
             <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              <Outlet />
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+              </div>
+              <div className="col-span-12 xl:col-span-8 mt-4 md:mt-6">
+                <Table />
+              </div>
             </div>
           </main>
-          {/* <!-- ===== Main Content End ===== --> */}
         </div>
-        {/* <!-- ===== Content Area End ===== --> */}
       </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
     </div>
   );
 };
