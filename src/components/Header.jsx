03fb80d01beef8352df59f0903740c225069e36c
charleslukes <charleslukes28@gmail.com>
2023-09-29 04:44:59 +0100

    add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,43 +3,43 @@ import Logo from "../assets/logo1.png";
 
 const Header = (props) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
-        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+    <header class="sticky top-0 z-999 flex w-full bg-white shadow-md h-20 drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+      <div class="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
+        <div class="flex items-center gap-2 sm:gap-4 lg:hidden">
           <button
             aria-controls="sidebar"
             onClick={(e) => {
               e.stopPropagation();
               props.setSidebarOpen(!props.sidebarOpen());
             }}
-            className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+            class="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
           >
-            <span className="relative block h-5.5 w-5.5 cursor-pointer">
-              <span className="du-block absolute right-0 h-full w-full">
+            <span class="relative block h-5.5 w-5.5 cursor-pointer">
+              <span class="du-block absolute right-0 h-full w-full">
                 <span
-                  className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                  class={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen() && "!w-full delay-300"
                   }`}
                 ></span>
                 <span
-                  className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                  class={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen() && "delay-400 !w-full"
                   }`}
                 ></span>
                 <span
-                  className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                  class={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen() && "!w-full delay-500"
                   }`}
                 ></span>
               </span>
-              <span className="absolute right-0 h-full w-full rotate-45">
+              <span class="absolute right-0 h-full w-full rotate-45">
                 <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
+                  class={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen() && "!h-0 !delay-[0]"
                   }`}
                 ></span>
                 <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
+                  class={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen() && "!h-0 !delay-200"
                   }`}
                 ></span>
@@ -48,17 +48,17 @@ const Header = (props) => {
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
 
-          <A className="block flex-shrink-0 lg:hidden" href="/">
+          <A class="block flex-shrink-0 lg:hidden" href="/">
             <img class="h-10 w-10" src={Logo} alt="Logo" />
           </A>
         </div>
 
-        <div className="hidden sm:block">
+        <div class="hidden sm:block">
           <form action="https://formbold.com/s/unique_form_id" method="POST">
-            <div className="relative">
-              <button className="absolute top-1/2 left-0 -translate-y-1/2">
+            <div class="relative">
+              <button class="absolute top-1/2 left-0 -translate-y-1/2">
                 <svg
-                  className="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
+                  class=""
                   width="20"
                   height="20"
                   viewBox="0 0 20 20"
@@ -83,13 +83,13 @@ const Header = (props) => {
               <input
                 type="text"
                 placeholder="Type to search..."
-                className="w-full bg-transparent pr-4 pl-9 focus:outline-none"
+                class="w-full bg-transparent pr-4 pl-9 focus:outline-none"
               />
             </div>
           </form>
         </div>
 
-        <div className="flex items-center gap-3 2xsm:gap-7"></div>
+        <div class="flex items-center gap-3 2xsm:gap-7"></div>
       </div>
     </header>
   );
