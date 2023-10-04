568d3bd64a872bb274d8aef646ed98c5ef70ee79
charleslukes <charleslukes28@gmail.com>
2023-10-04 05:33:35 +0100

    add login api and move to pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,32 @@
 import { A, useNavigate } from "@solidjs/router";
+import { createSignal, Show } from "solid-js";
 import Logo from "../assets/logo1.png";
+import { loginUser } from "../service";
+import { Ok } from "../service/constants";
+import ErrorAlert from "../components/alert/ErrorAlert";
+
 
 function Login() {
   const navigate = useNavigate();
+  const [loginApiResp, setLoginApiResp] = createSignal(null);
 
-  const handleSubmit = () => {
-    navigate("/home", { replace: true });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target[0].value;
+    const res = await loginUser(username);
+    const { status, message } = await res.json();
+    if (res.status === Ok) {
+      navigate("/home", { replace: true });
+    } else {
+      setLoginApiResp({
+        title: status,
+        details: message,
+      });
+
+      setTimeout(() => {
+        setLoginApiResp(null);
+      }, 3000);
+    }
   };
 
   return (
@@ -17,6 +38,13 @@ function Login() {
       <div class="text-gray-500 text-sm">Welcome back, you've been missed</div>
 
       <form class="w-full" onsubmit={handleSubmit}>
+        <Show when={loginApiResp()} fallback={<></>}>
+          <ErrorAlert
+            title={loginApiResp().title}
+            details={loginApiResp().details}
+            handleClick={() => setLoginApiResp(null)}
+          />
+        </Show>
         <div class="my-3">
           <label
             for="username"
@@ -26,6 +54,7 @@ function Login() {
           </label>
           <div class="mb-2">
             <input
+              required
               type="text"
               name="username"
               id="username"
@@ -35,25 +64,6 @@ function Login() {
             />
           </div>
         </div>
-
-        {/* <div class="my-3">
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Email address
-          </label>
-          <div class="mb-2">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autocomplete="email"
-              placeholder="abc@email.com"
-              class="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div> */}
 
         <button
           type="submit"
