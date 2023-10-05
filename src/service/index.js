f04224dce9f7517a4fabc05834346ce4c75b7ac1
charleslukes <charleslukes28@gmail.com>
2023-10-05 02:19:51 +0100

    add service

--- src/service/index.js
+++ src/service/index.js
@@ -12,7 +12,7 @@ export const register = async (body) => {
 
     return res;
   } catch (error) {
-    console.log('error ==>', error);
+    console.log("error ==>", error);
   }
 };
 
@@ -21,7 +21,45 @@ export const loginUser = async (user_name) => {
     const res = await fetch(`${base_url}/user/${user_name}`);
     return res;
   } catch (error) {
-    console.log('error ==>', error);
+    console.log("error ==>", error);
   }
 };
 
+export const getUser = async (user_name) => {
+  try {
+    const res = await fetch(`${base_url}/user/${user_name}`);
+    const {
+      data: { user },
+    } = await res.json();
+    let cards = [];
+    if (user) {
+      cards = [
+        {
+          title: user.user_name,
+          detail: "Username",
+        },
+        {
+          title: user.ref_code,
+          detail: "Personal invite code",
+        },
+        {
+          title: user.added_by_ref_code,
+          detail: "Total via code",
+        },
+      ];
+    }
+    return cards;
+  } catch (error) {
+    console.log("error ==>", error);
+  }
+};
+
+export const allUsers = async () => {
+  try {
+    const res = await fetch(`${base_url}/users`);
+    const data = await res.json();
+    return data.users ?? [];
+  } catch (error) {
+    console.log("error ==>", error);
+  }
+};
