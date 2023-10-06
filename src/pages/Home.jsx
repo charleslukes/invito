815d9b31c4f2f8b25da78812597c84a866dcc751
charleslukes <charleslukes28@gmail.com>
2023-10-06 21:30:44 +0100

    use location in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import Sidebar from '../components/Sidebar';
-import DefaultLayout from '../layout/DefaultLayout';
+import { useLocation } from "@solidjs/router";
+import Sidebar from "../components/Sidebar";
+import DefaultLayout from "../layout/DefaultLayout";
 
 const Home = () => {
+  const location = useLocation();
+  const { username } = location.state;
   return (
     <div>
-        <DefaultLayout />
+      <DefaultLayout username={username} />
     </div>
   );
 };
